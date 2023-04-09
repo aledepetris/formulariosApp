365a6de9eb8cd6bec97ba12058f56c590735a338
Alejandro Depetris <aledepetris@outlook.com>
2023-04-09 20:47:49 -0300

    Sidemenu y Template Basicos - Se creo el componente de sidemenu para navegación. - Se completo componente básico, utilizando validaciones por template y directivas personalizadas.

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  initForm = {
+    producto: '',
+    precio: 0,
+    existencias: 0
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // guardar( miFormulario: NgForm ) {
+  guardar () {
+    this.miFormulario.resetForm({
+      producto: '',
+      precio: 0,
+      existencias: 0
+    });
+  }
+
+  nombreValido(): boolean {
+    return this.miFormulario?.controls['producto']?.invalid &&
+           this.miFormulario?.controls['producto']?.touched
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls['precio']?.value < 0 &&
+           this.miFormulario?.controls['precio']?.invalid &&
+           this.miFormulario?.controls['precio']?.touched
+
   }
 
 }
