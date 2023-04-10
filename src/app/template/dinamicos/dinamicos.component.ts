3a9aaca3e804b7a8e8b6030d62edcc6ce21be5e4
Alejandro Depetris <aledepetris@outlook.com>
2023-04-09 22:25:17 -0300

    Forms Dinámicos - Finalización del componente Template dinámicos.

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +18,38 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  persona: Persona = {
+    nombre: 'Alejandro',
+    favoritos: [
+      {id:1, nombre: 'Cs GO'},
+      {id:2, nombre: 'DOTA 2'}
+    ]
+  }
 
-  ngOnInit(): void {
+  nuevoJuego: string = '';
+
+  agregarJuego () {
+
+    const maxIndex = this.persona.favoritos.length - 1
+
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos[maxIndex].id + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push( {...nuevoFavorito} );
+    this.nuevoJuego = '';
+
+  }
+
+  eliminar ( index: number ) {
+    this.persona.favoritos.splice(index, 1)
+  }
+
+  guardar() {
+
   }
 
 }
